d99283fcf369a04201fe47055a6196fd31c8890b
Makson047 <bogatyuk1998@gmail.com>
2022-12-15 19:43:48 +0200

    added Homework #11

---
+++ HW_11/classes.py
@@ -0,0 +1,109 @@
+from collections import UserDict
+from datetime import datetime
+
+
+class Field:
+    def __init__(self, value: str):
+        self.__value = None
+        self.value = value
+
+    def __repr__(self):
+        return self.value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value: str):
+        if not all((value.startswith('+'), len(value) == 13, value[1:].isdigit())):
+            raise ValueError("Please, enter the phone number in format: '+380111111111'")
+        self.__value = value
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    def __str__(self) -> str:
+        return datetime.strftime(self.__value, '%d.%m.%Y')
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        try:
+            date_birthday = datetime.strptime(value, '%d.%m.%Y')
+            self.__value = date_birthday
+        except:
+            raise ValueError("Please, enter the birthday date in format: 'dd.mm.yyyy' (example 01.01.1990)")
+
+
+class AddressBook(UserDict):
+
+    def add_contact(self, name: Name, phone: Phone = None, birthday: Birthday = None):
+        contact = Record(name=name, phone=phone, birthday=birthday)
+        self.data[name.value] = contact
+
+    def add_record(self, record: 'Record'):
+        self.data[record.name.value] = record
+
+    def iterator(self, page_number, num_of_records):
+        new_data = list(self.data.items())
+        total_items = page_number * num_of_records
+        yield list(new_data[(total_items - num_of_records):total_items])
+
+
+class Record:
+    def __init__(self, name: Name, phone: Phone = None, birthday: Birthday = None):
+        self.name: Name = name
+        self.phones: list[Phone] = [phone] if phone is not None else []
+        self.birthday = birthday
+
+    def __repr__(self):
+        return f'{self.name.value}: {" ".join(phone.value for phone in self.phones)}'
+
+    def add_phone(self, phone_number: Phone):
+        self.phones.append(phone_number)
+
+    def change_phone(self, old_number: Phone, new_number: Phone):
+        try:
+            self.delete_phone(old_number)
+            self.add_phone(new_number)
+        except ValueError:
+            return f'{old_number} does not exist'
+
+    def delete_phone(self, old_phone: Phone):
+        for phone in self.phones:
+            try:
+                if phone.value == old_phone:
+                    self.phones.remove(phone)
+            except ValueError:
+                print(f"\nPhone number: {phone} doesn't exist")
+        # try:
+        #     self.phones.remove(phone)
+        # except ValueError:
+        #     return f'{phone} does not exist'
+
+    def add_birthday(self, birthday : Birthday):
+        self.birthday = birthday
+
+    def days_to_birthday(self):
+        if self.birthday:
+            today = datetime.now().date()
+            if self.birthday.value.replace(year=today.year) >= today:
+                result = (self.birthday.value.replace(year=today.year) - today).days
+            else:
+                result = (self.birthday.value.replace(year=today.year + 1) - today).days
+            return result
+        else:
+            return "If you want to know which days left to contact's birthday, then use command: 'set birthday' "
---
+++ HW_11/parser.py
@@ -0,0 +1,116 @@
+from handler import handlers
+from string import ascii_letters
+
+
+def parser_handler(func):
+    def wrapper(user_command: str):
+        try:
+            return func(user_command)
+        except ValueError as e:
+            return str(e)
+        except KeyError as e:
+            return str(e)
+
+    return wrapper
+
+
+def hello_parser(user_command: str):
+    return 'hello', []
+
+
+def add_parser(user_command: str):
+    args = user_command.lstrip('add ')
+    user_name, phone_number = args.strip().split(' ')
+    if user_name == '':
+        raise ValueError('Bad input name!')
+    # elif not correct_phone(phone_number):
+    #     raise ValueError('Bad input phone number!')
+    else:
+        return 'add', [user_name, phone_number]
+
+
+def change_parser(user_command: str):
+    args = user_command.lstrip('change ')
+    user_name, new_phone_number = args.strip().split(' ')
+    if user_name == '':
+        raise ValueError('Bad input name!')
+    # elif not correct_phone(phone_number):
+    #     raise ValueError('Bad input phone number!')
+    else:
+        return 'change', [user_name, new_phone_number]
+
+
+def phone_parser(user_command: str):
+    user_name = user_command.strip().lstrip('phone ')
+    if user_name == '':
+        raise ValueError('Bad input name!')
+    else:
+        return 'phone', [user_name]
+
+
+def show_all_parser(user_command: str):
+    if user_command == 'show all ':
+        return 'show all', []
+    else:
+        raise ValueError('Bad input!')
+
+
+def set_birthday_parser(user_command: str):
+    args = user_command.lstrip('set birthday ')
+    user_name, birthday = args.strip().split(' ')
+    if user_name == '':
+        raise ValueError('Bad input name!')
+    else:
+        return 'set birthday', [user_name, birthday]
+
+
+def show_birthday_parser(user_command: str):
+    user_name = user_command.strip().lstrip('show birthday ')
+    if user_name == '':
+        raise ValueError('Bad input name!')
+    else:
+        return 'show birthday', [user_name]
+
+
+def close_parser(user_command: str):
+    for item in ['good bye ', 'close ', 'exit ']:
+        if item == user_command:
+            return 'close', []
+    raise ValueError('Bad input!')
+
+
+command_parser = {
+    "hello": hello_parser,
+    "add": add_parser,
+    "change": change_parser,
+    "phone": phone_parser,
+    "show all": show_all_parser,
+    "set birthday": set_birthday_parser,
+    "show birthday": show_birthday_parser,
+    "good bye": close_parser,
+    "close": close_parser,
+    "exit": close_parser
+}
+
+
+# def correct_phone(phone):
+#     if len(phone) < 10 or len(phone) > 17:
+#         print('Phone must be 10-17 characters without letters')
+#         return False
+#     for i in phone[0]:
+#         if i in ascii_letters:
+#             print('Phone must be 10-17 characters without letters')
+#             return False
+#
+#     return True
+
+
+@parser_handler
+def user_command_parser(user_command: str) -> tuple[str, list]:
+    for command in command_parser.keys():
+        normalized_input = ' '.join(list(filter(lambda x: x != '', user_command.split(' '))))
+        normalized_input = normalized_input.ljust(len(normalized_input) + 1, ' ')
+        if normalized_input.startswith(command + ' '):
+            parser = command_parser.get(command)
+            return parser(user_command=normalized_input)
+    raise ValueError('Unknown command! Please enter another command')
